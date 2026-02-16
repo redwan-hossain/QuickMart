635c98b49fab2ac0d06eb1582c8814df0dc6d941
redwan-hossain <rfredwan0@gmail.com>
2026-02-16 19:24:12 +0600

    all product api integrated

--- script/script.js
+++ script/script.js
@@ -7,6 +7,20 @@ function loadChooseData() {
 }
 
 
+
+const loadAllProducts = () => {
+    const url = "https://fakestoreapi.com/products";
+    fetch(url)
+        .then(res => res.json())
+        .then(allProduct => {
+            displayAllProduct(allProduct);
+        })
+}
+
+
+// all display function here
+
+
 const displayChooseData = (chooseData) => {
 
     const cartContainer = document.querySelector(".cart-container");
@@ -31,4 +45,51 @@ const displayChooseData = (chooseData) => {
 }
 
 
+const displayAllProduct = (allProduct) => {
+
+
+    const trendingCartContainer = document.querySelector(".trending-cart-container");
+    trendingCartContainer.innerHTML = "";
+
+    for (product of allProduct) {
+        trendingCartContainer.innerHTML += `
+       
+       <div class="trending-cart px-5 rounded outline outline-gray-400">
+
+                    <div class="bg-[#E5E7EB] flex justify-center items-center p-5">
+                        <img class="mx-auto"
+                            src="${product.image}" alt="">
+                    </div>
+
+                    <div class="px-3 mt-3">
+                        <div class="flex justify-between">
+                            <span class="rounded-lg bg-[#E0E7FF] text-[#645DC2] px-2 font-bold">${product.category}</span>
+
+                            <div class="flex items-center gap-2">
+                                <i class="fa-solid fa-star text-[#FDC701]"></i>
+                                <p>${product.rating.rate}</p>
+                                <span>(${product.rating.count})</span>
+                            </div>
+
+                        </div>
+
+                        <div class="space-y-1 mt-2">
+                            <h1 class="text-lg font-semibold">${product.title}</h1>
+                            <span class="font-bold ">$${product.price}</span>
+                            <div class="btnDiv flex mt-3 gap-4 justify-between pb-[10px]">
+
+                                <button class="border px-5 py-1 rounded-md cursor-pointer"><i class="fa-regular fa-eye mx-2"></i>Details</button>
+                                <button class="border px-5 py-1 rounded-md cursor-pointer"><i class="fa-solid fa-cart-shopping mx-2"></i>add</button>
+                               
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+       
+       `
+    }
+}
+
 loadChooseData();
+loadAllProducts()
